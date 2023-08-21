b6549c0c90923cc980d3ae50cd231e4cfe12826d
bguizado <brando.guizado.87@gmail.com>
2023-08-21 00:34:38 -0500

    Creando el model de la tabla usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Usuarios(models.Model):
+    
+    DNI = "DNI"
+    CARNET = "CARNET EXT."
+    PASAPORTE = "PASAPORTE"
+    TipoDocumento = [
+        (DNI, "Documento de Identidad"),
+        (CARNET, "Carnet de Extranjería"),
+        (PASAPORTE, "Pasaporte"),
+    ]
+    
+    GESTOR = "GESTOR",
+    SUPERVISOR = "SUPERVISOR",
+    ANALISTA = "ANALISTA",
+    CONTROLLER = "CONTROLLER"
+    TipoUsuario = [
+        (GESTOR, "Gestor"),
+        (SUPERVISOR, "Supervisor"),
+        (ANALISTA, "Analista"),
+        (CONTROLLER, "Controller")
+
+    ]
+
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=150)
+    primerApellido = models.CharField(max_length=150, db_column='apellido_paterno')
+    segundoApellido = models.CharField(max_length=150, db_column='apellido_materno')
+    tipoDocumento = models.CharField(max_length=11, choices=TipoDocumento, db_column='tipo_documento')
+    numeroDocumento = models.IntegerField(unique=True)
+    tipoUsuario = models.CharField(max_length=10, choices=TipoUsuario, db_column='tipo_usuario')
+    correo = models.EmailField(max_length=254, unique=True)
+    celular = PhoneNumberField(null=False, blank=False)
+    fechaCreacion = models.DateTimeField(auto_now_add=True, db_column='fecha_creacion')
+    fechaModifacion = models.DateTimeField(auto_now=True, db_column='fecha_modificacion')
+    estaActivo = models.BooleanField(default=True, db_column="esta_activo")
+
+    class Meta:
+        db_table = 'usuarios'
